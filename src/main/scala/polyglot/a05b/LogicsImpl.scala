69340de75d5fc1478759443e23bbee1e662be7c9
Lorenzo Rigoni <lorenzo.rigoni2@studio.unibo.it>
2025-03-31 15:43:56 +0200

    Implemented first LogicImpl exercise

--- src/main/scala/polyglot/a05b/LogicsImpl.scala
+++ src/main/scala/polyglot/a05b/LogicsImpl.scala
@@ -1,11 +1,24 @@
 package polyglot.a05b
 
 import polyglot.a05b.Logics
+import scala.util.Random
+import scala.math.abs
 
 /** solution and descriptions at https://bitbucket.org/mviroli/oop2019-esami/src/master/a05b/sol2/ */
 class LogicsImpl(private val size: Int) extends Logics:
-  override def tick(): Unit = {}
+  private val rnd: Random = Random()
+  private val initialX: Int = rnd.nextInt(size - 2) + 1
+  private val initialY: Int = rnd.nextInt(size - 2) + 1
+  private var tickCount: Int = 0
 
-  override def isOver: Boolean = false
+  override def tick(): Unit = tickCount = tickCount + 1
 
-  override def hasElement(x: Int, y: Int): Boolean = false
+  override def isOver: Boolean = initialY - tickCount < 0 ||
+    initialY + tickCount >= size ||
+    initialX - tickCount < 0 ||
+    initialX + tickCount >= size
+
+  override def hasElement(x: Int, y: Int): Boolean = (x == initialX && abs(y - initialY) <= tickCount) ||
+    (y == initialY && abs(x - initialX) <= tickCount) ||
+    (x - y == initialX - initialY && abs(x - initialX) <= tickCount) ||
+    (x + y == initialX + initialY && abs(x - initialX) <= tickCount)
